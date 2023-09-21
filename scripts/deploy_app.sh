d2ca6ab2fa003259bdd0b90726479b4113b4ecdc
Daniel Slosky <danielslosky@gmail.com>
2023-09-21 10:06:31 -0700

    Initial commit

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,1 @@
+cdk deploy --app "python3 -m infrastructure.app" --profile dev --output ./infrastructure/app/cdk.out --require-approval never
---
+++ scripts/deploy_env.sh
@@ -0,0 +1,1 @@
+cdk deploy --app "python3 -m infrastructure.env" --profile dev --output ./infrastructure/env/cdk.out --require-approval never
